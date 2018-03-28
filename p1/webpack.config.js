c5db568740a7f1e4ecb06fa6c75dc67b4db08755
ghostchan <jinnchan@qq.com>
2018-03-28 09:20:41 +0800

    1-4 使用 Loader

--- p1/webpack.config.js
+++ p1/webpack.config.js
@@ -8,5 +8,13 @@ module.exports = {
     filename: 'bundle.js',
     // 输出文件都放到 dist 目录下
     path: path.resolve(__dirname, './dist'),
-  }
+  },
+    module:{
+      rules:[
+          {
+              test:/\.css$/,
+              use:['style-loader','css-loader?minimize']
+          }
+      ]
+    }
 };
